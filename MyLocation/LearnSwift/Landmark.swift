0e1cc98fda683cb1b7a88b5b165285760edd2b75
Kiren Paul <kirenpaul@gmail.com>
2023-03-23 17:16:05 +0700

    classs 2 resources added

---
+++ MyLocation/LearnSwift/Landmark.swift
@@ -0,0 +1,16 @@
+//
+//  Landmark.swift
+//  MyLocation
+//
+//  Created by Kiren Paul on 23/3/2566 BE.
+//
+
+import Foundation
+struct Landmark:Hashable, Codable {
+    var id : Int
+    var name : String
+    var park: String
+    var state: String
+    var description: String
+}
+    
