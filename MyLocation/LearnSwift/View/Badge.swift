5caf444a7251c497af2a1dff7fb3cb405d00ba24
Kiren Paul <kirenpaul@gmail.com>
2023-03-28 17:08:39 +0700

    Section 4 Combine the Badge Foreground and Background 1

---
+++ MyLocation/LearnSwift/View/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  MyLocation
+//
+//  Created by Kiren Paul on 28/3/2566 BE.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
