cf4d5dbdccad5322f40b6041cb3a94ade30f0715
Kiren Paul <kirenpaul@gmail.com>
2023-03-28 17:16:25 +0700

    expiriment with views

--- MyLocation/LearnSwift/View/CommonViews/TextView.swift
+++ MyLocation/LearnSwift/View/CommonViews/TextView.swift
@@ -14,6 +14,26 @@ struct TextView: View {
             //MapView(coordinate: ).ignoresSafeArea(edges:.top) .frame(height: 300)
            // CircleImage(image: "turtlerock").offset(y:-130).padding(.bottom, -130)
             VStack(alignment:.leading) {
+                ZStack {
+                   Circle().fill(.green)
+                   Circle().fill(.yellow).scaleEffect(0.8)
+                   Circle().fill(.orange).scaleEffect(0.6)
+                   Circle().fill(.red).scaleEffect(0.4)
+                }
+                
+                Path { path in
+                   path.move(to: CGPoint(x: 20, y: 0))
+                   path.addLine(to: CGPoint(x: 20, y: 200))
+                   path.addLine(to: CGPoint(x: 220, y: 200))
+                   path.addLine(to: CGPoint(x: 220, y: 0))
+                }
+                .fill(
+                   .linearGradient(
+                       Gradient(colors: [.green, .blue]),
+                       startPoint: .init(x: 0.5, y: 0),
+                       endPoint: .init(x: 0.5, y: 0.5)
+                   )
+                )
                 Text("Turtle Rock").font(.title);
                 HStack {
                     Text("Joshua Tree National Park").font(.subheadline)
