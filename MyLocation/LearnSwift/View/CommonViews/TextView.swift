ed1b7db405d2c74b6a52d0cc0a151fe5f6128315
Kiren Paul <kirenpaul@gmail.com>
2023-03-29 10:17:41 +0700

    adding more gradient views

--- MyLocation/LearnSwift/View/CommonViews/TextView.swift
+++ MyLocation/LearnSwift/View/CommonViews/TextView.swift
@@ -12,15 +12,37 @@ struct TextView: View {
     
         VStack {
             //MapView(coordinate: ).ignoresSafeArea(edges:.top) .frame(height: 300)
-           // CircleImage(image: "turtlerock").offset(y:-130).padding(.bottom, -130)
+           // CircleImage(image: "turtlerock").offset(y:-130).padding(.bottom, -130) vibgyor
             VStack(alignment:.leading) {
                 ZStack {
-                   Circle().fill(.green)
-                   Circle().fill(.yellow).scaleEffect(0.8)
-                   Circle().fill(.orange).scaleEffect(0.6)
-                   Circle().fill(.red).scaleEffect(0.4)
+                   Circle().fill(.purple)
+                    Circle().fill(.indigo).scaleEffect(0.9)
+                    Circle().fill(.blue).scaleEffect(0.75)
+                   Circle().fill(.green).scaleEffect(0.6)
+                   Circle().fill(.yellow).scaleEffect(0.45)
+                   Circle().fill(.orange).scaleEffect(0.3)
+                    Circle().fill(.red).scaleEffect(0.15)
                 }
-                
+                ZStack {
+                   Circle().fill(.red)
+                    Circle().fill(.orange).scaleEffect(0.9)
+                    Circle().fill(.yellow).scaleEffect(0.75)
+                   Circle().fill(.green).scaleEffect(0.6)
+                   Circle().fill(.blue).scaleEffect(0.45)
+                   Circle().fill(.indigo).scaleEffect(0.3)
+                    Circle().fill(.purple).scaleEffect(0.15)
+                }
+                ZStack {
+                   Circle().fill(.black)
+                    Circle().fill(.white).scaleEffect(0.9)
+                    Circle().fill(.red).scaleEffect(0.8)
+                    Circle().fill(.orange).scaleEffect(0.7)
+                    Circle().fill(.yellow).scaleEffect(0.6)
+                   Circle().fill(.green).scaleEffect(0.5)
+                   Circle().fill(.blue).scaleEffect(0.4)
+                   Circle().fill(.indigo).scaleEffect(0.3)
+                    Circle().fill(.purple).scaleEffect(0.2)
+                }
                 Path { path in
                    path.move(to: CGPoint(x: 20, y: 0))
                    path.addLine(to: CGPoint(x: 20, y: 200))
@@ -29,8 +51,8 @@ struct TextView: View {
                 }
                 .fill(
                    .linearGradient(
-                       Gradient(colors: [.green, .blue]),
-                       startPoint: .init(x: 0.5, y: 0),
+                    Gradient(colors: [.green, .blue, .yellow, .red]),
+                       startPoint: .init(x: 0.1, y: 0),
                        endPoint: .init(x: 0.5, y: 0.5)
                    )
                 )
